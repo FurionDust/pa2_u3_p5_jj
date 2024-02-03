c65a77c63dca0d1b44abeb2676fd40b2cd1bc6b9
Alejandro Jimenez <alejandro.jmzl33@gmail.com>
2024-02-02 20:17:45 -0500

    taller29

--- src/main/java/com/uce/edu/Pa2U3P5JjApplication.java
+++ src/main/java/com/uce/edu/Pa2U3P5JjApplication.java
@@ -1,20 +1,19 @@
 package com.uce.edu;
 
-import java.math.BigDecimal;
-import java.util.List;
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-import com.uce.edu.empleado.repository.modelo.Ciudadano;
 import com.uce.edu.empleado.service.ICiudadanoService;
-import com.uce.edu.hoteles.repository.modelo.Hotel;
 import com.uce.edu.hoteles.service.IHotelService;
-import com.uce.edu.libro.repository.model.Autor;
 import com.uce.edu.libro.service.IAutorService;
 import com.uce.edu.libro.service.ILibroService;
+import com.uce.edu.ventas.repository.modelo.Cliente;
+import com.uce.edu.ventas.repository.modelo.Factura;
 import com.uce.edu.ventas.service.IFacturaService;
 
 @SpringBootApplication
@@ -31,16 +30,6 @@ public class Pa2U3P5JjApplication implements CommandLineRunner {
 	@Autowired
 	private ICiudadanoService ciudadanoService;
 
-	// Join Types en jakarta persistance
-	// 1) JOIN
-	// 1.1) INNER JOIN
-	// 1.2) OUTER JOIN
-	// 1.2.1) RIGHT
-	// 1.2.2) LEFT
-	// 1.2.3) FULL
-	// 2) JOIN WHERE
-	// 3) FETCH JOIN
-
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U3P5JjApplication.class, args);
 	}
@@ -49,62 +38,19 @@ public class Pa2U3P5JjApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
-		
-		System.out.println("INNER JOIN");
-		//1)
-		List<Autor> lista = this.autorService.buscarPorNombreInnerJoin("Alejandro Loor");
-		for(Autor a: lista) {
-			System.out.println(a);
-		}
-		//2)
-		List<Hotel> lista2 = this.hotelService.buscarPorNombreInnerJoin("El templo");
-		for(Hotel h: lista2) {
-			System.out.println(h);
-		}
-		System.out.println("RIGHT JOIN");
-		//1)
-		List<Ciudadano> lista5 = this.ciudadanoService.buscarPorCedulaRightJoin("1719608729");
-		for(Ciudadano c: lista5) {
-			System.out.println(c);
-		}
-		//2)
-		List<Ciudadano> lista6 = this.ciudadanoService.buscarPorSalarioRightJoin(new BigDecimal(450));
-		for(Ciudadano ci: lista6) {
-			System.out.println(ci);
-		}
-		System.out.println("LEFT JOIN");
-		//1) 
-		List<Autor> lista3 = this.autorService.buscarPorNacionalidadLeftJoin("Chileno");
-		for(Autor au: lista3) {
-			System.out.println(au);
-		}
-		//2)
-		List<Autor> lista4 = this.autorService.buscarPorCedulaLeftJoin("171960829");
-		for(Autor aut: lista4) {
-			System.out.println(aut);
-		}
-		System.out.println("FULL JOIN");
-		//1)
-		List<Ciudadano> lista7 = this.ciudadanoService.buscarCiudadanosFullJoin();
-		for(Ciudadano c:lista7) {
-			System.out.println(c);
-		}
-		//2)
-		List<Autor> lista8 = this.autorService.buscarAutoresFullJoin();
-		for(Autor a:lista8) {
-			System.out.println(a);
-		}
-		System.out.println("FETCH JOIN");
-		//1)
-		List<Hotel> lista9 = this.hotelService.seleccionarHotelesFetchJoin();
-		for(Hotel h:lista9) {
-			System.out.println(h);
-		}
-		//2)
-		List<Ciudadano> lista10 =this.ciudadanoService.buscarCiudadanosFetchJoin();
-		for(Ciudadano ciu:lista10) {
-			System.out.println(ciu);
-		}
+		System.out.println(TransactionSynchronizationManager.isActualTransactionActive());
+
+		Factura fact = new Factura();
+		fact.setCedula("123123");
+		fact.setFecha(LocalDateTime.now());
+		fact.setNumero("001-002");
+
+		Cliente cli = new Cliente();
+		cli.setApellido("Perez");
+		cli.setNombre("Alejandro");
+
+		this.facturaService.guardar(fact, cli);
+
 	}
 
 }
--- src/main/java/com/uce/edu/ventas/service/IFacturaService.java
+++ src/main/java/com/uce/edu/ventas/service/IFacturaService.java
@@ -3,35 +3,35 @@ package com.uce.edu.ventas.service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.uce.edu.ventas.repository.modelo.Cliente;
 import com.uce.edu.ventas.repository.modelo.Factura;
 import com.uce.edu.ventas.repository.modelo.dto.FacturaDTO;
 
 public interface IFacturaService {
 
-	//CRUD 
+	// CRUD
 	public Factura buscarPorNumero(String numero);
-	
-	public void guardar(Factura factura);
-	
+
+	public void guardar(Factura factura, Cliente cliente);
+
 	public void borrar(Integer id);
-	
+
 	public int actualizarFechas(LocalDateTime fechaNueva, LocalDateTime fechaActual);
-	
+
 	public List<Factura> buscarFacturasInnerJoin();
 
 	public List<Factura> buscarFacturasRightJoin();
-	
+
 	public List<Factura> buscarFacturasLeftJoin();
-	
+
 	public List<Factura> buscarFacturasFullJoin();
-	
+
 	public List<Factura> buscarFacturasWhereJoin();
-	
+
 	public List<Factura> buscarFacturasFetchJoin();
-	
+
 	public int borrarPorNumero(String numero);
-	
-	public List<FacturaDTO> buscarFacturasDTO(); 
+
+	public List<FacturaDTO> buscarFacturasDTO();
 
 }
-
